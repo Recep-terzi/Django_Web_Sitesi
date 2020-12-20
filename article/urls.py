53ba86e6832854cb2b3418d09fe38c2fd24686ed
Recep-terzi <71466957+Recep-terzi@users.noreply.github.com>
2020-12-20 13:19:00 +0300

    django_blog_app

---
+++ article/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+app_name = "article"
+urlpatterns = [
+    path('dashboard/',views.dashboard,name = "dashboard"),
+    path('premium/',views.premium,name = "premium"),
+    path('checkout/',views.checkout,name = "checkout"),
+    path('addarticle/',views.addarticle,name = "addarticle"),
+    path('article/<int:id>',views.detail,name = "detail"),
+    path('update/<int:id>',views.updateArticle,name = "update"),
+    path('delete/<int:id>',views.deleteArticle,name = "delete"),
+    path('',views.articles,name = "articles"),
+    path('comment/<int:id>',views.addComment,name = "comment"),
+
+
+
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
